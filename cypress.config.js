663af0457af7a6243cc782e1e30a82cb331ed430
João Duque <bosco.duque@gmail.com>
2023-11-16 21:05:18 -0300

    feat: reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json",
+  },
   e2e: {
     baseUrl: 'https://serverest.dev',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
